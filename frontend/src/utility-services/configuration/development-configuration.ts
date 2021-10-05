d40807d6517ea75e1f408422de6bef252d905aea
Irina Tarnavski <irinata@microsoft.com>
2021-10-05 15:18:10 -0700

    Forgotten

--- frontend/src/utility-services/configuration/development-configuration.ts
+++ frontend/src/utility-services/configuration/development-configuration.ts
@@ -1,9 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT license.
 
-import { ConfigAuthentication, ConfigMaps, Configuration } from './configuration';
-
 import { SEVERITY } from '../logger';
+import { ConfigAuthentication, ConfigMaps, Configuration } from './configuration';
 
 export class DevelopmentConfiguration implements Configuration {
     readonly apiBaseUrl = '/api';
@@ -28,13 +27,13 @@ export class DevelopmentConfiguration implements Configuration {
         this.logLevel = logLevel || SEVERITY.DEBUG;
         this.authentication = {
             baseAuthUrl: baseAuthUrl || 'https://login.microsoftonline.com',
-            clientId: clientId || '27193f56-cb90-4e67-8cf2-13eabbbabca5',
-            tenant: tenant || 'ce2526f8-5105-481b-8e16-b2eb098b32bb',
+            clientId: clientId || 'input clientId here',
+            tenant: tenant || 'input tenantId here',
             TOKEN_PERSIST_KEY: TOKEN_PERSIST_KEY || 'MR_AUTH_TOKEN',
             NONCE_PERSIST_KEY: NONCE_PERSIST_KEY || 'MR_AUTH_TOKEN_NONCE',
         };
         this.maps = {
-            clientId: '13c1aae5-c721-459a-ac8b-0e1a973b99a6' || mapClientId || '34b48a29-3933-49df-9b58-0be37fea18a7'
+            clientId: mapClientId || 'input mapCLientId here'
         };
     }
 }
